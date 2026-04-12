dd525f9f382c58e77f5479f22380c224cde14d40
SUilhl3 <ilagang@my.centennialcollege.ca>
2026-04-12 04:31:51 -0400

    Timer script
    - has it set to a minute for now but needs to be changed later

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
-
+using TMPro;
 public class Timer : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float limitedTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,17 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (limitedTime > 0)
+        {
+            limitedTime -= Time.deltaTime;
+        }
+        else
+        {
+            limitedTime = 0;
+        }
         
+        int minutes = Mathf.FloorToInt(limitedTime / 60);
+        int seconds = Mathf.FloorToInt(limitedTime % 60);
+        timerText.text = string.Format("{00:00} : {1:00}", minutes, seconds);
     }
 }
